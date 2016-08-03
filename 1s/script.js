f75f141f389e0b3a31838984ceee402608986b18
19260817 <19260817@users.noreply.github.com>
2016-08-03 08:31:14 +0800

    Add $(window).load()

--- 1s/script.js
+++ 1s/script.js
@@ -1,35 +1,51 @@
 $(document).ready(function() {
-	function createCookie(name, value, days) {
-		if (days) {
-			var date = new Date();
-			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-			var expires = "; expires=" + date.toGMTString();
-		} else expires = "";
-		document.cookie = name + "=" + value + expires + "; path=/";
-	}
+  function createCookie(name, value, days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      var expires = "; expires=" + date.toGMTString();
+    } else expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
 
-	function readCookie(name) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for (var i = 0; i < ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-		}
-		return null;
-	}
-	$("#xuming-trigger").click(function(e) {
-		e.preventDefault();
-		var isProlonged = readCookie("prolonged");
-		if (!isProlonged) {
-			createCookie("prolonged", true, 1);
-			$('#prolonged').openModal();
-		} else {
-			$('#failed').openModal();
-		}
-	});
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  $("#xuming-trigger").click(function(e) {
+    e.preventDefault();
+    var isProlonged = readCookie("prolonged");
+    if (!isProlonged) {
+      createCookie("prolonged", true, 1);
+      $('#prolonged').openModal();
+    } else {
+      $('#failed').openModal();
+    }
+  });
 });
 
 $(window).load(function() {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-45383141-2','auto');ga('send','pageview');
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-45383141-2', 'auto');
+  ga('send', 'pageview');
+  $('.preloader-wrapper').fadeOut();
+  $('#preloader').delay(350).fadeOut('slow');
+  $('body').delay(350).css({
+    'overflow': 'visible'
+  });
 })
